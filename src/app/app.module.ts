5b4ae4bdeb21be1e7ac0f1e6008a41dae3b6f653
Valentin Carnaru <valikonen@gmail.com>
2017-09-24 11:13:08 +0300

    Add prime-ng library

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { DataGridModule } from 'primeng/primeng';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    DataGridModule
   ],
   providers: [],
   bootstrap: [AppComponent]
